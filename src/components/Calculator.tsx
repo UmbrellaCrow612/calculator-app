2701021f8c04c95daed629ebe5e2b13665dbbf8a
yousaf wazir <umbrellaman612@gmail.com>
2023-02-27 13:48:45 +0000

    made fully function cal mobile

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,56 +1,69 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function Calculator() {
   const [number, setNumber] = useState("0");
-  const calc = (input: string) => {
-    switch (input) {
-      case "DEL":
-        // remove the last character from the number string
-        setNumber((prevNumber) =>
-          prevNumber.length === 1 ? "0" : prevNumber.slice(0, -1)
-        );
-        break;
-      case "Reset":
-        // reset the number to 0
-        setNumber("0");
-        break;
-      case "=":
-        // evaluate the expression and update the number state
-        try {
-          setNumber(eval(number).toString());
-        } catch (error) {
-          setNumber("Error");
-        }
-        break;
-      default:
-        // append the input to the current number string
-        setNumber((prevNumber) =>
-          prevNumber === "0" ? input : prevNumber + input
-        );
-        break;
-    }
-  };
 
+  const calc = useCallback(
+    (input: any) => {
+      switch (input) {
+        case "+/-":
+          setNumber((prevNumber) =>
+            prevNumber.startsWith("-") ? prevNumber.slice(1) : `-${prevNumber}`
+          );
+          break;
+        case "C":
+          setNumber("0");
+          break;
+        case "DEL":
+          setNumber((prevNumber) =>
+            prevNumber.length > 1 ? prevNumber.slice(0, -1) : "0"
+          );
+          break;
+        case ".":
+          if (!number.includes(".")) {
+            setNumber((prevNumber) => `${prevNumber}.`);
+          }
+          break;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+          setNumber((prevNumber) => `${prevNumber} ${input} `);
+          break;
+        case "=":
+          try {
+            const result = eval(number);
+            setNumber(result.toString());
+          } catch (error) {
+            setNumber("Error");
+          }
+          break;
+        default:
+          setNumber((prevNumber) =>
+            prevNumber === "0" ? input : prevNumber + input
+          );
+      }
+    },
+    [number]
+  );
 
-  const Key = ({
-    children,
-    isBottomKeys,
-  }: {
-    children: string;
-    isBottomKeys?: boolean;
-  }) => {
-    return (
-      <button
-        onClick={() => calc(children)}
-        className={`btn btn-accent font-extrabold text-xl ${
-          isBottomKeys ? "col-span-2" : ""
-        }`}
-      >
-        {children}
-      </button>
-    );
-  };
+  const Key = useCallback(
+    ({ children, isBottomKeys, isFinal }: any) => {
+      return (
+        <button
+          onClick={() => calc(children)}
+          className={`kbd font-extrabold text-xl ${
+            isBottomKeys ? "col-span-2" : ""
+          } ${isFinal ? "col-span-4" : ""}`}
+        >
+          {children}
+        </button>
+      );
+    },
+    [calc]
+  );
+
   return (
     <div className="w-4/5 flex flex-col gap-6 min-h-[80vh]">
       {/* Top start*/}
@@ -73,21 +86,22 @@ export default function Calculator() {
         <Key>7</Key>
         <Key>8</Key>
         <Key>9</Key>
-        <Key>DEL</Key>
+        <Key>/</Key>
         <Key>4</Key>
         <Key>5</Key>
         <Key>6</Key>
-        <Key>+</Key>
+        <Key>*</Key>
         <Key>1</Key>
         <Key>2</Key>
         <Key>3</Key>
         <Key>-</Key>
         <Key>.</Key>
         <Key>0</Key>
-        <Key>/</Key>
-        <Key>x</Key>
-        <Key isBottomKeys>Reset</Key>
-        <Key isBottomKeys>=</Key>
+        <Key>C</Key>
+        <Key>+</Key>
+        <Key isBottomKeys>DEL</Key>
+        <Key isBottomKeys>+/-</Key>
+        <Key isFinal>=</Key>
       </div>
     </div>
   );
